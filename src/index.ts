5e35e607a32b472efed516fdd45d9eabe9b94e96
psykologist90 <ericcobossanchez@gmail.com>
2017-06-06 08:46:06 +0200

    Removed unnecessary css

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ import 'bootstrap/dist/js/bootstrap';
 
 import "font-awesome/css/font-awesome.css";
 import "bootstrap/dist/css/bootstrap.css";
-import "angularjs-slider/dist/rzslider.css";
 import "./styles/screen.scss";
 
 angular.module("app", ["app.application"]);
